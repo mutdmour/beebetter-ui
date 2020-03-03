00a2cd2537cbc3560bad51fb7bd8af5bbeb15aff
mutdmour <mutdmour@gmail.com>
2020-03-03 23:13:23 +0200

    add dot for required fields

--- src/components/form/elements/RadioGroup.vue
+++ src/components/form/elements/RadioGroup.vue
@@ -1,17 +1,26 @@
 <template>
- <b-form-group :label="label">
-  <b-form-radio
-   v-for="(opt, index) in options"
-   v-bind:key="index"
-   v-on:input="updateValue"
-   name="radios"
-   v-model="selected"
-   :value="opt.value"
-   :required="required"
-  >
-   {{ opt.label }}
-  </b-form-radio>
- </b-form-group>
+ <b-row>
+  <b-form-group>
+   <b-col>
+    <label>{{ label }}</label>
+    <b-icon v-show="required" icon="dot" variant="danger"></b-icon>
+   </b-col>
+
+   <b-col>
+    <b-form-radio
+     v-for="(opt, index) in options"
+     v-bind:key="index"
+     v-on:input="updateValue"
+     name="radios"
+     v-model="selected"
+     :value="opt.value"
+     :required="required"
+    >
+     {{ opt.label }}
+    </b-form-radio>
+   </b-col>
+  </b-form-group>
+ </b-row>
 </template>
 
 <script lang="ts" src="./RadioGroup.ts"></script>
--- src/components/form/elements/TextInput.vue
+++ src/components/form/elements/TextInput.vue
@@ -1,16 +1,24 @@
 <template>
- <b-form-group :label="label">
-  <b-form-input
-   v-model="textValue"
-   type="text"
-   v-on:input="updateValue"
-   size="lg"
-   :required="required"
-   :state="asExpected"
-   @blur="doneTyping"
-   @update="onUpdate"
-  />
- </b-form-group>
+ <b-row>
+  <b-form-group>
+   <b-col>
+    <label>{{ label }}</label>
+    <b-icon v-show="required" icon="dot" variant="danger"></b-icon>
+   </b-col>
+   <b-col>
+    <b-form-input
+     v-model="textValue"
+     type="text"
+     v-on:input="updateValue"
+     size="lg"
+     :required="required"
+     :state="asExpected"
+     @blur="doneTyping"
+     @update="onUpdate"
+    />
+   </b-col>
+  </b-form-group>
+ </b-row>
 </template>
 
 <script lang="ts" src="./TextInput.ts"></script>
